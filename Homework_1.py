8e6520d9597117fda78ea99cfcaf9a5036539e27
Vadym Nedoinov <vadim.nedoynov@gmail.com>
2019-12-14 12:30:39 +0200

    Start second task, height_weight

--- Homework_1.py
+++ Homework_1.py
@@ -9,5 +9,10 @@ def generator():
     return '<br />'.join(f"User name: {Faker().name()}, Email: {Faker().email()}" for i in range(100))
 
 
+@app.route('/')
+def height_weight():
+    return
+
+
 if __name__ == '__main__':
     app.run()
