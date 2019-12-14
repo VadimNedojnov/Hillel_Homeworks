abab3c14beb29afe24a984edaaab56edaefc8ed5
Vadym Nedoinov <vadim.nedoynov@gmail.com>
2019-12-14 13:32:18 +0200

    Finished second task: height_weight

--- Homework_1.py
+++ Homework_1.py
@@ -9,9 +9,19 @@ def generator():
     return '<br />'.join(f"User name: {Faker().name()}, Email: {Faker().email()}" for i in range(100))
 
 
-@app.route('/')
+@app.route('/height_weight')
 def height_weight():
-    return
+    with open('hw.csv') as file:
+        height = 0
+        weight = 0
+        count = 0
+        for line in file.readlines():
+            modern_line = line.strip().split(', ')
+            if len(modern_line) == 3 and modern_line[1][0].isdigit() and modern_line[2][0].isdigit():
+                height += float(modern_line[1])
+                weight += float(modern_line[2])
+                count += 1
+    return f'Average height: {height / count}; Average weight: {weight / count}'
 
 
 if __name__ == '__main__':
